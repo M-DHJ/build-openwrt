230baefd7e282d5865252e538cd631b18fef12b5
M-DHJ <3145255636@qq.com>
2025-11-11 19:40:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.2.9/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的luci显示的固件名称
-sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
 
 # 添加额外的软件包
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
